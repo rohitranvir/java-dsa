987bc1cd7796524b4a789737de7ae9f5c7b56ed6
Rohit <rohitranveer358@gmail.com>
2025-04-06 03:39:25 +0530

    functional overloading

--- Bionomial.java
+++ Bionomial.java
@@ -1,7 +1,7 @@
 public class Bionomial {
     public static int factorial(int num) {
         int fact = 1;
-        for (int i = 2; i <= num; i++) {
+        for (int i = 1; i <= num; i++) {
             fact *= i;
         }
         return fact;
