0e5b278e9e348e428cffbe296863a0b9747a314c
Rohit <rohitranveer358@gmail.com>
2025-04-06 02:51:02 +0530

    binomial

--- Bionomial.java
+++ Bionomial.java
@@ -1,8 +1,20 @@
 public class Bionomial {
+    public static int factorial(int num) {
+        int fact = 1;
+        for (int i = 2; i <= num; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
     public static int bincoeff(int n,int r){
-return n;
+        int fact_n=factorial(n);
+        int fact_r=factorial(r);
+        int fact_n_minus_r=factorial(n-r);
+        int bincoeff=fact_n/(fact_r*fact_n_minus_r);
+return bincoeff;
     }
     public static void main(String[] args) {
         int bin=bincoeff(5,4);
+        System.out.println(bin);
     }
 }
